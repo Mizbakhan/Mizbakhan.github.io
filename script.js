fc4d18f94e9357dd84b363113bdbe8c70340e025
Mizba Khan M <119849545+Mizbakhan@users.noreply.github.com>
2024-02-20 14:03:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function openSkills() {
+    window.open("skills.html", "_self");
+  }
+  
+  function openCertifications() {
+    window.open("certifications.html", "_self");
+  }
+  
+  function openEducation() {
+    window.open("education.html", "_self");
+  }
+  
+  function openProjects() {
+    window.open("projects.html", "_self");
+  }
+ 
